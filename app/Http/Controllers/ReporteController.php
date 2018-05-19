f0b199c2fd762880d2c3542cb36163aa15d1ad9e
Erick Alberto Castrillo Arroyo <erick.castrillo@hpe.com>
2018-05-18 20:09:35 -0600

    Integrated stock report

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -133,7 +133,52 @@ class ReporteController extends Controller
         // If filters are empty, we proceed to get all products from the given Almacen
         if(empty($filtros)) {
             if ($request->input('ajaxData.almacen') == -1) {
-                return response()->json("Todas la bodegas");
+                $fechaInicio = $request->input('ajaxData.fechaInicio');
+                $fechaFinal = $request->input('ajaxData.fechaFinal');
+                $raw = "SELECT 
+                            almacenes_detalle.articulo_id AS 'ID Artitulo',
+                            almacenes_detalle.cantidad AS 'Cantidad',
+                            almacenes_detalle.costo_unitario AS 'Costo Unitario',
+                            monedas.nombre AS 'Moneda',
+                            articulos.codigo AS 'Codigo de Producto',
+                            articulos.descripcion AS 'Descripcion',
+                            unidades_medidas.nombre AS 'Unidad de Medida',
+                            articulos.cantidad_minima AS 'Cantidad Minima',
+                            almacenes_detalle.lote AS 'Lote',
+                            almacenes_detalle.serie AS 'Serie',
+                            almacenes_detalle.pais AS 'Pais'
+                        FROM
+                            almacenes_detalle
+                                INNER JOIN
+                            articulos ON almacenes_detalle.articulo_id = articulos.id
+                                INNER JOIN
+                            unidades_medidas ON articulos.medida_id = unidades_medidas.id
+                                INNER JOIN
+                            monedas ON almacenes_detalle.moneda_id = monedas.id
+                        WHERE
+                            almacenes_detalle.estado = 1 AND
+                            almacenes_detalle.created_at between '{$fechaInicio}' AND '{$fechaFinal}';";
+                $data = DB::select($raw);
+                $headers = [
+                    'ID Artitulo',
+                    'Cantidad',
+                    'Costo Unitario',
+                    'Moneda',
+                    'Codigo de Producto',
+                    'Descripcion',
+                    'Unidad de Medida',
+                    'Cantidad Minima',
+                    'Lote',
+                    'Serie',
+                    'Pais'
+                ];
+
+                $result = [
+                    "headers" => $headers,
+                    "data" => $data,
+                ];
+
+                return response()->json($result);
             } else {
                 $id = $request->input('ajaxData.almacen');
                 $fechaInicio = $request->input('ajaxData.fechaInicio');
